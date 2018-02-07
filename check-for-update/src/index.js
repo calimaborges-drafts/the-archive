88473d1f033e6231120034ef6c86de626c8c0490
Carlos Augusto Borges <calimaborges@gmail.com>
2018-02-07 09:00:09 -0200

    little improvement on check-for-updates;9C

--- check-for-update/src/index.js
+++ check-for-update/src/index.js
@@ -7,7 +7,12 @@ const check_for_update = ({ fetch_content, check_for_news }) => async () => {
   const body = await fetch_content();
   console.log("Checking for news...");
   const data = await check_for_news(body);
-  if (data) notify(data);
+  if (data) {
+    console.log("NEWS!!");
+    notify(data);
+  } else {
+    console.log("Nothing new :-(");
+  }
 };
 
 const VERIFY_INTERVAL = 10 * 1000;
